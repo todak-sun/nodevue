55126a89d75eb3634a5ac2e1f840383f6738796a
todak_sun <tjsdydwn@gmail.com>
2019-08-25 17:12:38 +0900

    채팅구현(미완)
    58:05초까지 시청... 이어서 볼것!!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-    app = express();
+    app = express(),
+    util = require('util');
 
 const Pool = require('./pool'),
     Mydb = require('./mydb');
@@ -31,4 +32,61 @@ app.get('/dbtest/:name', (req, res) => {
 
 const server = app.listen(3000, function () {
     console.log("Express's started on port 3000");
+});
+
+const io = require('socket.io').listen(server, {
+    log: false,
+    origins: '*:*',
+    pingInterval: 3000,
+    pingTimeout: 5000
+});
+
+io.sockets.on('connection', (socket, option) => {
+    socket.emit('message', {
+        msg: "Welcome " + socket.id
+    });
+
+    util.log("connection>>", socket.id, socket.handshake.query);
+
+    socket.on('join', (roomId, fn) => {
+        socket.join(roomId, () => {
+            util.log("Join", roomId, Object.keys(socket.rooms));
+        });
+        if (fn) {
+            fn();
+        }
+    });
+
+    socket.on('leave', (roomId, fn) => {
+        socket.leave(roomId, () => {
+            if (fn) {
+                fn();
+            }
+        });
+    });
+
+    socket.on('rooms', (fn) => {
+        if (fn) {
+            fn(Object.keys(socket.rooms));
+        }
+    });
+
+    socket.on('message', (data, fn) => {
+        util.log("message >> ", data.msg, Object.keys(socket.rooms));
+        if (fn) {
+            fn(data.msg);
+        }
+        socket.broadcast.to(data.room).emit('message', {
+            room: data.room,
+            msg: data.msg
+        });
+    });
+
+    socket.on('disconnecting', (data) => {
+        util.log("disconnecting>>", socket.id);
+    });
+
+    socket.on('disconnect', (data) => {
+        util.log('disconnect>>>', socket.id, Object.keys(socket.rooms));
+    })
 });
